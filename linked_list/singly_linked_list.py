a6ca84b7c4cdd9ebd511d1266b6e0a7aec03f633
Rafal Zebrowski <rafal.zebrowski@gmail.com>
2021-10-04 14:10:51 +0200

    file rename

--- linked_list/singly_linked_list.py
+++ linked_list/singly_linked_list.py
@@ -2,7 +2,7 @@ class Node:
     
     def __init__(self, data=None, next_node=None):
         self.data = data  # data
-        self.next_node = next_node  # pointer
+        self.next_node = next_node  # pointer (Node)
     
 
 class LinkedList:
@@ -12,11 +12,11 @@ class LinkedList:
     
     def insert_at_beginning(self, data):
         head_node = Node(data, self.head)  
-        self.head = head_node  # head node of the linked list 
+        self.head = head_node  # head node of the linked list (first index (0))
 
     def insert_at_end(self, data):
         if self.head is None:
-            self.head = Node(data, None)  # tail node of the linked list
+            self.head = Node(data, None)  # tail node of the linked list (last index (-1))
             return
 
         itr = self.head
@@ -86,6 +86,27 @@ class LinkedList:
             itr = itr.next_node
             counter += 1
 
+    def remove_at_data(self, data): 
+        if self.head.data == data:
+            self.head = self.head.next_node
+            return
+
+        itr = self.head
+        while itr:
+            if itr.next_node.data == data:
+                itr.next_node = itr.next_node.next_node
+                return 
+
+            itr = itr.next_node
+
+    def reverse(self):
+        length = self.__len__()
+
+        itr = self.head
+        for i in range(length):
+            self.insert_at_beginning(itr.data)
+            itr = itr.next_node
+
     def __repr__(self):
         """ Beginning- left side
             End- right side 
@@ -101,4 +122,16 @@ class LinkedList:
 
 
 # work in progress 
-# x = LinkedList()
+x = LinkedList()
+x.insert_at_beginning(2)
+x.insert_at_beginning(4)
+x.insert_at_beginning(6)
+x.insert_at_end(8)
+
+print(x)
+x.remove_at_data(6)
+print(x)
+print(len(x))
+print(x)
+x.reverse()
+print(x)
