7648f3e52e8aa59f529d906eebe3d5a5f375f4de
Rafal Zebrowski <rafal.zebrowski@gmail.com>
2021-10-24 01:16:53 +0200

    print_level

--- tree/general_tree.py
+++ tree/general_tree.py
@@ -18,26 +18,22 @@ class TreeNode:
         return level
 
     def print_tree(self):
-        level = self.get_level() * ' ' * 4 
-        print(f"-{level + '|___' if self.parent else ' ' * 4}{self.data}")
+        current_print_level = self.get_level() * ' ' * 4 
+        print(f"-{current_print_level + '|___' if self.parent else ' ' * 4}{self.data}")
         
         if self.children:
             for child in self.children:
                 child.print_tree()
-
-      
-# {self.parent.data if self.parent else 'no parent'}
-x10 = TreeNode(10)
-x8 = TreeNode(8)
-x9 = TreeNode(9)
-x7 = TreeNode(7)
-x10.add_child(x8)
-x10.add_child(x9)
-x8.add_child(x7)
-x4 = TreeNode(4)
-x12 = TreeNode(12)
-x8.add_child(x4)
-x4.add_child(x12)
-
-x10.print_tree()
-print(x10.get_level())
+    
+    def print_tree_level(self, level, show_parents=False):
+        
+        if self.get_level() == level:
+            if show_parents:
+                print(f"{self.parent.data if self.parent else 'None'}\
+                     \n|___{self.data}")
+            else:
+                print(f"{self.data}")
+        
+        if self.children:
+            for child in self.children:
+                child.print_tree_level(level, show_parents)
